7daba2584a324b1296488c85dc75b1d01432c750
claudiocassimiro <days_surfing@live.com>
2021-12-13 17:28:46 -0300

    Adiciona logica de saudaçao de acordo com o horario da visita

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import AppContext from '../context/AppContext';
 import Header from '../components/Header';
-import Main, { Section, ContainerThings1, Article, Title, Paragraph, ContainerImg, Img } from './styles/HomeStyle';
+import {
+  Section,
+  SectionAll,
+  ContainerThings1,
+  Article,
+  TecArticle,
+  Title,
+  Paragraph,
+  ContainerImg,
+  Img,
+} from './styles/HomeStyle';
+import SpiderMan from '../img/spiderman.png';
 import IconHtml from '../img/html5.png';
 import IconCss from '../img/css3.png';
 import IconGit from '../img/git.png';
@@ -12,46 +23,82 @@ import IconRtl from '../img/rtl.png';
 function Home() {
 
   const { name } = useContext(AppContext);
+  const [greeting, setGreeting] = useState('');
+
+  useEffect(() => {
+    function getHour() {
+      const date = new Date();
+      const hour = date.getHours();
+      if (hour >= 0 && hour <= 5) {
+        setGreeting('Boa madrugada!');
+      }
+
+      if (hour >= 6 && hour < 12) {
+        setGreeting('Bom dia!');
+      }
+
+      if (hour >= 12 && hour <= 18) {
+        setGreeting('Boa tarde!');
+      }
+
+      if (hour >= 18) {
+        setGreeting('Boa noite!');
+      }
+    }
+    getHour();
+  }, []);
 
   return (
-    <Main>
+    <main>
       <Header />
       <Section>
-        <h1>{`Olá ${name}, seja bem vindx ao meu portifólio`}</h1>  
+        <h1>{`Olá ${name}, ${greeting} =D`}</h1>  
       </Section>
-      <ContainerThings1>
-        <Article>
-          <Title>Meu nome é Cláudio, e...</Title>
-          <Paragraph>Lorem Ipsum is simply dummy text of the printing and 
-            typesetting industry. Lorem Ipsum has been the industry's 
-            standard dummy text ever since the 1500s, when an unknown 
-            printer took a galley of type and scrambled it to make a 
-            type specimen book. It has survived not only five centuries, 
-            but also the leap into electronic typesetting, remaining 
-            essentially unchanged. It was popularised in the 1960s with 
-            the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus 
-            PageMaker including versions of Lorem Ipsum.
-          </Paragraph>
-        </Article>
-        <Article>
-          <Title>Tecnologias:</Title>
-          <ContainerImg>
-            <Img src={IconHtml} alt ='html icone'/>
-            <Img src={IconCss} alt ='css icone'/>
-            <Img src={IconGit} alt ='git icone'/>
-            <Img src={IconJS} alt ='js icone'/>
-            <Img src={IconReact} alt ='react icone'/>
-            <Img src={IconRtl} alt ='rtl icone'/>
-          </ContainerImg>
-        </Article>
-      </ContainerThings1>
-      <div>
-        <article>
-          <img src={'#'} alt='Minha foto' />
-        </article>
-      </div>
-    </Main>
+      <SectionAll>
+        <ContainerThings1>
+          <Article>
+            <Title>Meu nome é Cláudio, e...</Title>
+            <Paragraph>Lorem Ipsum is simply dummy text of the printing and 
+              typesetting industry. Lorem Ipsum has been the industry's 
+              standard dummy text ever since the 1500s, when an unknown 
+              printer took a galley of type and scrambled it to make a 
+              type specimen book. It has survived not only five centuries, 
+              but also the leap into electronic typesetting, remaining 
+              essentially unchanged. It was popularised in the 1960s with 
+              the release of Letraset sheets containing Lorem Ipsum passages, 
+              and more recently with desktop publishing software like Aldus 
+              PageMaker including versions of Lorem Ipsum.
+              Lorem Ipsum is simply dummy text of the printing and 
+              typesetting industry. Lorem Ipsum has been the industry's 
+              standard dummy text ever since the 1500s, when an unknown 
+              printer took a galley of type and scrambled it to make a 
+              type specimen book. It has survived not only five centuries, 
+              but also the leap into electronic typesetting, remaining 
+              essentially unchanged. It was popularised in the 1960s with 
+              the release of Letraset sheets containing Lorem Ipsum passages, 
+              and more recently with desktop publishing software like Aldus 
+              PageMaker including versions of Lorem Ipsum.
+            </Paragraph>
+          </Article>
+          <TecArticle>
+            <Title>Tecnologias:</Title>
+            <ContainerImg>
+              <Img src={IconHtml} alt='html icone'/>
+              <Img src={IconCss} alt='css icone'/>
+              <Img src={IconGit} alt='git icone'/>
+              <Img src={IconJS} alt='js icone'/>
+              <Img src={IconReact} alt='react icone'/>
+              <Img src={IconRtl} alt='rtl icone'/>
+            </ContainerImg>
+          </TecArticle>
+        </ContainerThings1>
+        <div>
+          <article>
+            <img src={SpiderMan} alt='Minha foto' />
+          </article>
+        </div>
+      </SectionAll>
+    </main>
   );
 }
 
