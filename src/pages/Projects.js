501748dfdfd12d903993fae4eaa99fdd5abfd7c4
claudiocassimiro <days_surfing@live.com>
2021-12-13 17:42:22 -0300

    Cria page para os projetos

---
+++ src/pages/Projects.js
@@ -0,0 +1,12 @@
+function Projects() {
+  return (
+    <main>
+      <h1>Projects</h1>
+      <section>
+        { /* Aqui vão ficar os cards com os projetos */ }
+      </section>
+    </main>
+  );
+}
+
+export default Projects;
