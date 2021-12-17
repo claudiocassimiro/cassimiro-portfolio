fd7cac8cce114dc3fd8685fa9e149dbd7723e5ee
claudiocassimiro <days_surfing@live.com>
2021-12-17 17:25:11 -0300

    Adiciona alguns elementos necessarios para a page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,33 @@
+import Header from "../components/Header";
+import { SectionTitle, SectionCards, Cards } from "./styles/Projects";
+
 function Projects() {
   return (
     <main>
-      <h1>Projects</h1>
-      <section>
-        { /* Aqui vão ficar os cards com os projetos */ }
-      </section>
+      <Header />
+      <SectionTitle>
+        <h1>Projects</h1>
+      </SectionTitle>
+      <SectionCards>
+        <Cards>
+          <span>Projeto-1</span>
+        </Cards>
+        <Cards>
+          <span>Projeto-2</span>
+        </Cards>
+        <Cards>
+          <span>Projeto-3</span>
+        </Cards>
+        <Cards>
+          <span>Projeto-4</span>
+        </Cards>
+        <Cards>
+          <span>Projeto-5</span>
+        </Cards>
+        <Cards>
+          <span>Projeto-6</span>
+        </Cards>
+      </SectionCards>
     </main>
   );
 }
