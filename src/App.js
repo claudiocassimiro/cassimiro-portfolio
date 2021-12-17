af0577cc6734ca788d134b8d37080d48902661f4
claudiocassimiro <days_surfing@live.com>
2021-12-17 17:24:46 -0300

    Adiciona rota para a page de projetos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Login from "./pages/Login";
 import { Route, Switch } from "react-router-dom";
 import ProvideContext from "./context/ProvideContext";
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Login} />
         <Route path="/home" component={Home} />
+        <Route path="/projects" component={Projects} />
       </Switch>
     </ProvideContext>
   );
