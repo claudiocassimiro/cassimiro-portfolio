86b49c575808359d88b504b465465d20cb3a123d
claudiocassimiro <days_surfing@live.com>
2021-12-17 17:25:29 -0300

    Cria arquivo de estilo para a page de projetos

---
+++ src/pages/styles/Projects.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const SectionTitle = styled.section`
+  text-align: center;
+  margin: 50px 0 50px 0;
+`;
+
+export const SectionCards = styled.section`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  align-items: center;
+  width: 50%;
+  height: 700px;
+  margin: 0 auto;
+`;
+
+export const Cards = styled.article`
+  width: 400px;
+  height: 330px;
+  border: 1px solid #ccc;
+`; 
