3733cda6b06726ba7124be85264363e79b8da969
claudiocassimiro <days_surfing@live.com>
2021-12-18 10:47:47 -0300

    Adiciona estilo para os cards

--- src/pages/styles/Projects.js
+++ src/pages/styles/Projects.js
@@ -16,7 +16,34 @@ export const SectionCards = styled.section`
 `;
 
 export const Cards = styled.article`
+  display: flex;
+  flex-flow: column;
+  justify-content: space-evenly;
+  align-items: center;
   width: 400px;
   height: 330px;
   border: 1px solid #ccc;
-`; 
+  text-align: center;
+`;
+
+export const ProjectTitle = styled.h2`
+`;
+
+export const Img = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+
+export const Button = styled.button`
+  width: 100px;
+  height: 30px;
+  border-radius: 3px;
+  outline: none;
+  border: none;
+  background-color: #4397D5;
+  transition: background-color 0.2s;
+  &:hover {
+    cursor: pointer;
+    background-color: #2F6CAD;
+  }
+`;
