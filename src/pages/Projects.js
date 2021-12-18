5189d087b851b3c3862c078dffdeb28cc6136a89
claudiocassimiro <days_surfing@live.com>
2021-12-18 10:47:25 -0300

    Adiciona template para os futuros projetos

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,31 +1,44 @@
 import Header from "../components/Header";
-import { SectionTitle, SectionCards, Cards } from "./styles/Projects";
+import { SectionTitle, SectionCards, Cards, Img, ProjectTitle, Button } from "./styles/Projects";
+import Construcao from "../img/construcao.png";
 
 function Projects() {
   return (
     <main>
       <Header />
       <SectionTitle>
-        <h1>Projects</h1>
+        <h1>Projetos</h1>
       </SectionTitle>
       <SectionCards>
         <Cards>
-          <span>Projeto-1</span>
+          <ProjectTitle>Projeto-1</ProjectTitle>
+          <Img src={Construcao} alt="projeto 1" />
+          <Button>Ir para projeto</Button>
         </Cards>
         <Cards>
-          <span>Projeto-2</span>
+          <ProjectTitle>Projeto-2</ProjectTitle>
+          <Img src={Construcao} alt="projeto 2" />
+          <Button>Ir para projeto</Button>
         </Cards>
         <Cards>
-          <span>Projeto-3</span>
+          <ProjectTitle>Projeto-3</ProjectTitle>
+          <Img src={Construcao} alt="projeto 3" />
+          <Button>Ir para projeto</Button>
         </Cards>
         <Cards>
-          <span>Projeto-4</span>
+          <ProjectTitle>Projeto-4</ProjectTitle>
+          <Img src={Construcao} alt="projeto 4" />
+          <Button>Ir para projeto</Button>
         </Cards>
         <Cards>
-          <span>Projeto-5</span>
+          <ProjectTitle>Projeto-5</ProjectTitle>
+          <Img src={Construcao} alt="projeto 5" />
+          <Button>Ir para projeto</Button>
         </Cards>
         <Cards>
-          <span>Projeto-6</span>
+          <ProjectTitle>Projeto-6</ProjectTitle>
+          <Img src={Construcao} alt="projeto 6" />
+          <Button>Ir para projeto</Button>
         </Cards>
       </SectionCards>
     </main>
