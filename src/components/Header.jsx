c5f82826b39ddb3ffeff7f64fce9d003fdd45f8e
claudiocassimiro <days_surfing@live.com>
2021-12-18 10:45:04 -0300

    Adiciona template para os futuros projetos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,21 @@
 /* eslint-disable no-unused-vars */
-import { Link as a } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import IconCC from '../img/icon.png';
-import HeaderStyle, { Section, ImgStyle, ListContainer, ListItem, Link as A } from './styles/HeaderStyle';
+import './styles/NavStyleLink.css';
+import HeaderStyle, { Section, ImgStyle, ListContainer, ListItem } from './styles/HeaderStyle';
 
 function Header() {
-  return ( 
+  return (
     <HeaderStyle>
       <Section>
         <a href="https://www.linkedin.com/in/claudiocassimiro/" target="_blank" rel="noreferrer">
           <ImgStyle src={IconCC} alt="Claudio Cassimiro" />
         </a>
         <ListContainer>
-          <ListItem><A href="/home">Home</A></ListItem>
-          <ListItem><A href="/projects">Projects</A></ListItem>
-          <ListItem><A href="/aboutme">About-me</A></ListItem>
-          <ListItem><A href="contacts">Contacts</A></ListItem>
+          <ListItem><Link className="link" to="/home">Home</Link></ListItem>
+          <ListItem><Link className="link" to="/projects">Projects</Link></ListItem>
+          <ListItem><Link className="link" to="/aboutme">About-me</Link></ListItem>
+          <ListItem><Link className="link" to="contacts">Contacts</Link></ListItem>
         </ListContainer>
       </Section>
     </HeaderStyle>
