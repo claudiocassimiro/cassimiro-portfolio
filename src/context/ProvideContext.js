11b22564500a34c56e7fc7a920f6e78ada01f681
claudiocassimiro <days_surfing@live.com>
2021-12-20 16:04:33 -0300

    Adiciona logica para receber frases de uma api

--- src/context/ProvideContext.js
+++ src/context/ProvideContext.js
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import AppContext from "./AppContext";
+import getPhrase from '../helpers/getPhrase';
 
 const ProvideContext = ({ children }) => {
   const [name, setName ] = useState('');
+  const [phrase, setPhrase] = useState('');
+  const [author, setAuthor] = useState('');
+
+  useEffect(() => {
+    getPhrase().then(({ text, author }) => {
+      setPhrase(text);
+      setAuthor(author);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }, []);
 
   return (
-    <AppContext.Provider value={{ name, setName }}>
+    <AppContext.Provider value={{ name, setName, phrase, author }}>
       {children}
     </AppContext.Provider>
   );
