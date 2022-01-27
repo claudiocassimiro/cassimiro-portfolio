34c661f8b9eb2da48b665de41a6937a3ce90eab3
claudiocassimiro <days_surfing@live.com>
2022-01-26 23:50:03 -0300

    Adiciona novos componentes estilizados

--- src/pages/styles/HomeStyle.js
+++ src/pages/styles/HomeStyle.js
@@ -5,124 +5,77 @@ export const Main = styled.main`
 `;
 
 export const Section = styled.section`
-  display: flex;
-  justify-content: flex-end;
   width: 100%;
-  margin: 25px 0 40px -20px;
-`;
-
-export const Greeting = styled.h1`
-  background-color: #4397D5;
-  border-radius: 8px;
-  padding: 10px;
-  color: #222;
-`;
-
-export const PhraseSection = styled.section`
+  height: 85vh;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  width: 100%;
-  margin-top: 100px;
-  margin-bottom: 50px;
-  margin: 0 auto;
-  @media (max-width: 400px) {
-    margin-top: 70px;
-  }
-`;
-
-export const Phrase = styled.h3`
-  width: 80%;
-  text-align: center;
-  font-size: 1.5rem;
-`;
-
-export const Author = styled.p`
-  font-size: 1.2rem;
-  color: #bbb;
-  margin-top: 8px;
-  align-self: center;
-`;
-
-export const SectionAll = styled.section`
-  display: flex;
-  justify-content: space-evenly;
-  margin: 150px 80px 0 80px;
-  @media(min-width: 280px) and (max-width: 1024px) {
+  @media (min-width: 220px) and (max-width: 1024px) {
+    margin: 0 auto;
     flex-direction: column;
-    margin: 50px 40px 0 40px;
-  }
-  @media(max-width: 1024px) {
-    margin: 70px 40px 0 40px;
+    align-items: center;
+    justify-content: center;
   }
 `;
 
-export const ContainerThings1 = styled.div`
+export const Article = styled.section`
   display: flex;
   flex-direction: column;
-  width: 600px;
-  height: 100%;
-  @media(min-width: 280px) and (max-width: 1024px) {
-    width: 100%;
+  @media (min-width: 220px) and (max-width: 1024px) {
+    margin: 0 auto;
+    padding: 50px;
   }
 `;
 
-export const ContainerThings2 = styled.div`
-  width: 600px;
-  height: 100%;
-  @media(min-width: 280px) and (max-width: 1024px) {
-    width: 100%;
-  }
-`;
-
-export const Article = styled.article`
-  margin-bottom: 30px;
-  padding: 20px;
-  background-color: #eee;
-  border-radius: 10px;
-  @media(max-width: 1400px) {
-    height: 60%;
-  }
-`;
-
-export const TecArticle = styled.article`
-  height: 100%;
-  margin-left: 100px;
-  margin-bottom: 30px;
-  padding: 20px;
-  border-radius: 10px;
-  background-color: #eee;
-  @media(min-width: 280px) and (max-width: 1024px) {
+export const H1Name = styled.h1`
+  font-size: 3rem;
+  margin-left: 30px;
+  color: #eee;
+  @media (min-width: 220px) and (max-width: 1024px) {
+    font-size: 1.5rem;
+    align-self: center;
     margin-left: 0;
   }
+  @media (min-width: 1025px) and (max-width: 1680px) {
+    font-size: 2rem;
+  }
 `;
 
-export const Title = styled.h2`
-  margin-bottom: 15px;
-  text-align: center;
-  color: #333;
+export const H2Ocupation = styled.h2`
+  font-size: 4rem;
+  margin-left: 30px;
+  color: #ccc;
+  @media (min-width: 220px) and (max-width: 1024px) {
+    font-size: 1.5rem;
+    align-self: center;
+    margin-left: 0;
+  }
+  @media (min-width: 1025px) and (max-width: 1680px) {
+    font-size: 2.5rem;
+  }
 `;
 
-export const Paragraph = styled.p`
-  text-align: justify;
-  color: #555;
-  font-size: 16px;
-  line-height: 1.5;
-  margin-bottom: 15px;
-`;
-
-export const ContainerImg = styled.div`
-  width: 60%;
+export const ContainerImg = styled.article`
   display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  align-items: space-around;
-  margin: 0 auto;
+  align-items: center;
+  @media (min-width: 220px) and (max-width: 1024px) {
+    & img {
+      width: 150px;
+      height: 150px;
+    }
+  }
+  @media (min-width: 1025px) and (max-width: 1680px) {
+    & img {
+      width: 200px;
+      height: 200px;
+    }
+  }
 `;
 
-export const Img = styled.img`
-  width: 70px;
-  height: 70px;
-  margin: 10px;
+export const ControlsButton = styled.button`
+  border: none;
+  background: none;
+  width: 50px;
+  font-size: 2rem;
+  margin: 0 20px;
 `;
